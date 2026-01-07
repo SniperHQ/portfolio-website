bbe8dbebc42a204abd9d7e20fb6868f0c065bc53
Mcondisi Ntshangase <hendry91234@gmail.com>
2026-01-07 18:12:07 +0200

    Auto update for contact form

--- core/models.py
+++ core/models.py
@@ -112,11 +112,13 @@ class ContactInfo(models.Model):
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True, null=True)  # <-- allow null for now
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - {self.email}"
+
 
 
 # ---------------- Social Links ----------------
